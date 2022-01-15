9bb85857186f7dd62ce2e00dc59c2df73f45cfb1
danydodson <danydodson@gmail.com>
2022-01-15 17:34:21 -0600

    refactor: :recycle: updates

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-import { IconLogo, IconStar } from '.'
+import { IconStar } from '.'
 
 const Icon = ({ name }) => {
   switch (name) {
-    case 'Logo':
-      return <IconLogo />
     case 'Star':
       return <IconStar />
     default:
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,2 @@
 export { default as Icon } from './icon'
-export { default as IconLogo } from './logo'
 export { default as IconStar } from './star'
