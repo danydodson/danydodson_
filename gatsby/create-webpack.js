214e969af821124bed02a2eda29e6f92d116de91
danydodson <danydodson@gmail.com>
2022-01-14 23:26:22 -0600

    refactor :art: setup onCreateWebpack

--- gatsby/create-webpack.js
+++ gatsby/create-webpack.js
@@ -1,36 +1,45 @@
-// const path = require('path')
+const path = require('path')
 
-// const onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+const onCreateWebpackConfig = ({ getConfig, stage, loaders, actions }) => {
 
-//   if (stage === 'build-html' || stage === 'develop-html') {
-//     actions.setWebpackConfig({
-//       module: {
-//         rules: [
-//           { test: /animejs/, use: loaders.null() },
-//           { test: /miniraf/, use: loaders.null() },
-//           { test: /scrollreveal/, use: loaders.null() }
-//         ]
-//       }
-//     })
-//   }
+  const config = getConfig()
 
-//   actions.setWebpackConfig({
-//     resolve: {
-//       alias: {
-//         'assets/*': path.resolve(__dirname, 'src/assets/*'),
-//         'components/*': path.resolve(__dirname, 'src/components/*'),
-//         'config/*': path.resolve(__dirname, 'src/config/*'),
-//         'content/*': path.resolve(__dirname, 'src/content/*'),
-//         'fonts/*': path.resolve(__dirname, 'src/templates/*'),
-//         'hooks/*': path.resolve(__dirname, 'src/hooks/*'),
-//         'images/*': path.resolve(__dirname, 'src/images/*'),
-//         'pages/*': path.resolve(__dirname, 'src/pages/*'),
-//         'styles/*': path.resolve(__dirname, 'src/styles/*'),
-//         'templates/*': path.resolve(__dirname, 'src/templates/*'),
-//         'utilities/*': path.resolve(__dirname, 'src/utilities/*'),
-//       }
-//     }
-//   })
-// }
+  if (stage.startsWith('develop') && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
 
-// module.exports = onCreateWebpackConfig
+  if (stage === 'build-html' || stage === 'develop-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          { test: /animejs/, use: loaders.null() },
+          { test: /miniraf/, use: loaders.null() },
+          { test: /scrollreveal/, use: loaders.null() }
+        ]
+      }
+    })
+  }
+
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        'assets/*': path.resolve(__dirname, 'src/assets/*'),
+        'components/*': path.resolve(__dirname, 'src/components/*'),
+        'config/*': path.resolve(__dirname, 'src/config/*'),
+        'content/*': path.resolve(__dirname, 'src/content/*'),
+        'fonts/*': path.resolve(__dirname, 'src/templates/*'),
+        'hooks/*': path.resolve(__dirname, 'src/hooks/*'),
+        'images/*': path.resolve(__dirname, 'src/images/*'),
+        'pages/*': path.resolve(__dirname, 'src/pages/*'),
+        'styles/*': path.resolve(__dirname, 'src/styles/*'),
+        'templates/*': path.resolve(__dirname, 'src/templates/*'),
+        'utilities/*': path.resolve(__dirname, 'src/utilities/*'),
+      }
+    }
+  })
+}
+
+module.exports = onCreateWebpackConfig
