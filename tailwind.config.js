2c19304aed688007d9d00bfcb38f29a48124833f
danydodson <danydodson@gmail.com>
2022-01-19 05:06:10 -0600

    refactor :recycle: updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,23 @@
 module.exports = {
   theme: {
     extend: {
-
       colors: {
         electric: '#f19dff',
         ribbon: '#acc3ff',
       },
-
       backgroundColor: {
         primary: 'var(--bg-primary)',
         secondary: 'var(--bg-secondary)',
         tertiary: 'var(--color-primary)'
       },
-
       textColor: {
         primary: 'var(--text-primary)',
         secondary: 'var(--text-secondary)',
         tertiary: 'var(--color-primary)'
       },
-
       borderRadius: {
         xs: '1px',
       },
-
     }
   },
 
