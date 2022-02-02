544a8acfd7e5f025aec63eb24c22e06844dbd1ed
Dany Dodson <danydodson@gmail.com>
2022-02-02 03:49:03 -0600

    refactor: :recycle: updates

--- src/components/shared/pills.js
+++ src/components/shared/pills.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import tw, { styled } from 'twin.macro'
 
-const Pill = styled.span(() => [
-  tw`text-2xl inline-flex items-center`,
-  tw`px-4 h-10 m-1 mr-2.5 rounded-md first-of-type:ml-0`,
-])
-
 const Pills = ({ items }) => {
   return (
     (items || []).map(item => (
@@ -15,5 +10,10 @@ const Pills = ({ items }) => {
     ))
   )
 }
+
+const Pill = styled.span(() => [
+  tw`text-2xl inline-flex items-center`,
+  tw`px-4 h-10 m-1 mr-2.5 rounded-md first-of-type:ml-0`,
+])
 
 export default Pills
